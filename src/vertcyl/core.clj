e4e381a2f66f25876627364576d31416b35bd483
Nic West <nicwest.cb@gmail.com>
2017-02-25 16:59:27 +0000

    foo

--- src/vertcyl/core.clj
+++ src/vertcyl/core.clj
@@ -4,7 +4,7 @@
                                     difference scale]]
             [scad-clj.scad :refer [write-scad]]
             [vertcyl.row :refer [row-with-switches place-row normalise-row
-                                 row-cutter thickness]]
+                                 thickness]]
             )
   (:gen-class))
 
@@ -15,30 +15,9 @@
          #(->> (row-with-switches radius switch columns)
                (place-row radius switch %))
          (range rows))
-       (apply union)
-       (rotate (/ Math/PI 4) [0 0 -1])))
+       (apply union)))
+       ;(rotate (/ Math/PI 4) [0 0 -1])))
 
-(defn cross-bar
-  [x y z radius gap]
-
-  (union
-    (difference
-      (->> (cube (* radius 2) 6 6)
-           (translate [x y z]))
-      (->> (sphere (+ radius thickness))
-           (translate [(+ radius thickness (/ gap 2)) 0 0]))
-      (->> (sphere (+ radius thickness))
-           (translate [(- 0 radius thickness (/ gap 2)) 0 0]))
-
-      (->> (cylinder 5/4 (* radius 2))
-           (rotate (/ Math/PI 2) [0 1 0])
-           (translate [x y z])))))
-
-(defn screw-hole
-  [x y z radius gap]
-  (->> (cylinder 5/4 (* radius 2))
-       (rotate (/ Math/PI 2) [0 1 0])
-       (translate [x y z])))
 
 
 (def complete
@@ -46,27 +25,21 @@
         switch :mx
         columns 5
         rows 4
-        gap 30
-        ]
+        gap 30]
     (union
       (difference
         (union
-          (->> (row-set radius switch columns rows)
-               (translate [(- 0 radius thickness (/ gap 2)) 0 0]))
-          (->> (row-set radius switch columns rows)
-               (translate [(- 0 radius thickness (/ gap 2)) 0 0])
-               (scale [-1 1 1])))
+          (->> (row-set radius switch columns rows))
+          (->> (difference
+                 (cube 86 50 105)
+                 (cube 80 44 99)
 
-        (screw-hole 0 1 50 radius gap)
-        (screw-hole 0 1 -50 radius gap)
-        (screw-hole 0 89 32 radius gap)
-        (screw-hole 0 89 -32 radius gap)
-        )
-    (cross-bar 0 1 50 radius gap)
-    (cross-bar 0 1 -50 radius gap)
-    (cross-bar 0 89 32 radius gap)
-    (cross-bar 0 89 -32 radius gap)
-      )))
+                 )
+               (translate [31 100 0])
+               ))
+          ))))
+
+
 
 
 (defn render!
--- src/vertcyl/row.clj
+++ src/vertcyl/row.clj
@@ -7,18 +7,22 @@
 (def thickness 3)
 
 (defn single-hole
-  [radius step switch width height n]
+  [width height switch a x y]
+  (difference 
+    (->> (cube height thickness (+ width 1))
+         (rotate a [0 0 -1])
+         (translate [x y 0]))
+    (->> (cutter switch)
+         (rotate (/ Math/PI 2) [1 0 0])
+         (rotate a [0 0 -1])
+         (translate [x y 0]))))
+
+(defn place-hole
+  [f radius step switch width height n ]
   (let [a (* step n)
         x (* (Math/sin a) (- radius thickness))
         y (* (Math/cos a) (- radius thickness))]
-      (difference 
-        (->> (cube height thickness (+ width thickness thickness))
-             (rotate a [0 0 -1])
-             (translate [x y 0]))
-        (->> (cutter switch)
-             (rotate (/ Math/PI 2) [1 0 0])
-             (rotate a [0 0 -1])
-             (translate [x y 0])))))
+    (f width height switch a x y)))
 
 (defn get-column-step
   [radius switch]
@@ -47,16 +51,15 @@
   (let [[width height] (padding switch)
         step (get-column-step radius switch)]
     (->> 
-      (apply union (map #(single-hole radius step switch width height %) 
+      (apply union (map #(place-hole single-hole radius step switch width height %) 
                         (range columns)))
       (normalise-row radius columns step width height))))
 
 (defn get-row-step
   [radius switch]
-  (let [gap 5
-        [width _] (padding switch)
+  (let [[width _] (padding switch)
         circum (* Math/PI 2 radius)
-        n (/ circum (+ gap width))]
+        n (/ circum width)]
     (/ (* Math/PI 2) n)))
 
 (defn place-row
@@ -82,10 +85,6 @@
     (spit "out/row-with-switches.scad"
           (write-scad (row-with-switches radius switch columns)))
     (spit "out/row-single-hole.scad"
-          (write-scad (single-hole radius step switch width height n)))
-    (spit "out/row-single-cutter.scad"
-          (write-scad (single-cutter radius depth step width height n)))
-    (spit "out/row-cutter.scad"
-          (write-scad (row-cutter radius depth switch columns)))))
+          (write-scad (single-hole radius step switch width height n)))))
 
 (render!)
