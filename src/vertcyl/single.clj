8346e611c95ec3bb4ce1824846de7b59319eea61
Nic West <nicwest.cb@gmail.com>
2017-03-08 00:11:10 +0000

    foobar

--- src/vertcyl/single.clj
+++ src/vertcyl/single.clj
@@ -1,13 +1,13 @@
 (ns vertcyl.single
   (:require [scad-clj.model :refer [cube cylinder difference rotate translate
-                                    union scale hull *fn*]]
+                                    union scale hull *fn* sphere]]
             [scad-clj.scad :refer [write-scad]]))
 
-(def width 22)
-(def height 22)
-(def radius 140)
+(def width 21)
+(def height 21)
+(def radius 110)
 (def plate-thickness 3)
-(def wall-thickness 5.5)
+(def wall-thickness 3)
 (def wall-height 10)
 (def shell-width (+ width wall-thickness))
 (def shell-height (+ height wall-thickness))
@@ -52,10 +52,15 @@
 
 
 (def single-finger-plate
-  (difference
-    shell-block
-    plate-cutter
-    switch-cutter))
+  (union
+    (difference
+      shell-block
+      plate-cutter
+      switch-cutter)
+    (->> (cylinder 0.5 15)
+         (translate [0 (+ (/ plate-thickness 2) radius) 0])
+         (rotate (/ Math/PI 2) [0 1 0])
+         )))
 
 (def single-thumb-plate
   (difference
@@ -65,7 +70,6 @@
 
 (defn place-switch
   [row column block]
-  (println *step*)
   (let [a (+ *offset* (* column *step*))
         b (+ (/ *offset* 2) (* row *offset*))
         x (* (Math/sin a) radius)
@@ -90,15 +94,109 @@
       (place-switch -1 0.5 single-finger-plate)
       (place-switch 0 0.5 single-finger-plate)))
 
-(def hand
-  (union
-    (->> fingers-plate
-         (rotate (/ Math/PI 64) [0 -1 0]))
-    (->> thumb-plate
+(def shell
+  ;(union 
+    
+    
+  (difference
+    (union
+      (difference 
+        (->> (difference
+               (sphere (+ (* radius 2) plate-thickness ))
+               (sphere (* radius 2)))
+             (scale [1 1 0.75]))
+        (->> (sphere (+ (* radius 2) plate-thickness))
+             (scale [1 1 0.75])
+             (rotate (/ Math/PI 3) [0 0 -1])
+             (rotate (/ Math/PI 6) [0 1 0])
+             (translate [-240 150 115])))
+      (difference
+        (->> (difference
+               (sphere (+ (* radius 2) plate-thickness))
+               (sphere (* radius 2)))
+             (scale [1 1 0.75])
+             (rotate (/ Math/PI 3) [0 0 -1])
+             (rotate (/ Math/PI 6) [0 1 0])
+             (translate [-240 150 115]))
+        (->> (sphere (+ (* radius 2)))
+             (scale [1 1 0.75])))
+
+    (->> (cube shell-thickness 150 200)
+         (translate [-79 240 0]))
+    (->> (cube 150 shell-thickness 200)
+         (translate [-20 204 0]))
+    )
+    (->> (sphere (* radius 2))
+         (scale [1 1 0.75])
          (rotate (/ Math/PI 3) [0 0 -1])
          (rotate (/ Math/PI 6) [0 1 0])
-         (translate [-280 160 155])
-         )))
+         (translate [-240 150 115]))
+
+    (->> (sphere (* radius 2))
+         (scale [1 1 0.75]))
+
+    (->> (cube (* radius 4) (* radius 6) (* radius 6))
+         (translate [-300 150 115]))
+    (->> (cube (* radius 6) (* radius 6) (* radius 6))
+         (translate [0 -127 115]))
+    (->> (cube (* radius 6) (* radius 6) (* radius 6))
+         (translate [293 0 384]))
+    (->> (cube (* radius 6) (* radius 6) (* radius 6))
+         (translate [374 0 0]))
+    (->> (cube (* radius 6) (* radius 6) (* radius 3))
+         (translate [0 0 -219]))
+    (->> (cube (* radius 6) (* radius 6) (* radius 6))
+         (translate [0 616 0]))
+    (->> (cube (* radius 4) (* radius 4.35) (* radius 6))
+         (rotate (/ Math/PI 3) [0 0 -1])
+         (rotate (/ Math/PI 6) [0 1 0])
+         (translate [0 0 419]))
+    (->> (cube (* radius 6) (* radius 6) (* radius 6))
+         (rotate (/ Math/PI 3) [0 0 -1])
+         (rotate (/ Math/PI 6) [0 1 0])
+         (translate [0 723 0]))
+         )
+  )
+;)
+
+(def shell-cutter
+  (union plate-cutter switch-cutter))
+
+(def finger-cutter
+  (union
+    (for [row (range (- (/ finger-rows 2)) (/ finger-rows 2))
+          column (range (- (/ finger-columns 2)) (/ finger-columns 2))]
+      (place-switch row column shell-cutter))))
+
+(def thumb-cutter
+  (union
+    (for [row (range (- (/ thumb-vert-rows 2)) (/ thumb-vert-rows 2))]
+      (place-switch row 1.50 shell-cutter))
+      (place-switch -1 0.5 shell-cutter)
+      (place-switch 0 0.5 shell-cutter)))
+
+(def hand
+  ;(union
+    ;(->> fingers-plate
+         ;;(rotate (/ Math/PI 64) [0 -1 0]))
+         ;)
+    ;(->> thumb-plate
+         ;(rotate (/ Math/PI 3) [0 0 -1])
+         ;(rotate (/ Math/PI 6) [0 1 0])
+         ;(translate [-240 150 115]))
+    (difference
+      shell
+    finger-cutter
+    (->> thumb-cutter
+         (rotate (/ Math/PI 3) [0 0 -1])
+         (rotate (/ Math/PI 6) [0 1 0])
+         (translate [-240 150 115]))
+
+
+    )
+   )
+  ;)
+;)
 
 
 (defn render-part!
