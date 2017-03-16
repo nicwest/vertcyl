7103c3766bafa710b58a0791170fdf538b7ff145
Nic West <nicwest.cb@gmail.com>
2017-03-16 22:04:29 +0000

    messing around with thumb posiition

--- src/vertcyl/single.clj
+++ src/vertcyl/single.clj
@@ -6,27 +6,32 @@
 (def width 21)
 (def height 21)
 (def radius 110)
-(def plate-thickness 5)
-(def wall-thickness 12)
+(def plate-thickness 3)
+(def wall-thickness 10)
 (def finger-rows 4)
 (def finger-columns 5)
 (def thumb-upper-rows 3)
 (def thumb-lower-rows 2)
 (def cirum (* Math/PI 2 radius))
-(def step (/ (* Math/PI 2) (/ cirum width)))
-(def offset (/ step 2))
+(def step-u10 (/ (* Math/PI 2) (/ cirum width)))
+(def step-u15 (/ (* Math/PI 2) (/ cirum (* width 1.5))))
+(def offset-u10 (/ step-u10 2))
+(def offset-u15 (/ step-u15 2))
 (def screw-radius 1)
 (def mounting-post-height 30)
 (def mounting-post-radius 3)
 (def shell-thickness 15)
-(def shell-gap 5)
 (def shell-height 90)
+(def thumb-x -30)
+(def thumb-y 35)
+(def thumb-z 20)
+(def shell-gap 10)
 
 (def switch-cutter
-  (let [extra (cube 3.5 15.6 14)
+  (let [extra (cube 3.5 15.6 40)
         angle (/ Math/PI 2)]
     (->> (union
-           (cube 14 14 14)
+           (cube 14 14 40)
            (translate [4.0 0 0] extra)
            (translate [-4.0 0 0] extra)
            (->> extra
@@ -37,18 +42,23 @@
                 (translate [0 -4 0])))
          (rotate angle [-1 0 0]))))
 
-(def wall-plate
+(def wall-plate-u10
   (->> (cube (+ width plate-thickness) (+ height plate-thickness) wall-thickness)
        (rotate (/ Math/PI 2) [-1 0 0])
        (translate [0 (- (/ wall-thickness 2)) 0])))
 
-(def wall-plate-15
-  (->> (cube (+ width plate-thickness) (* height 1.5) wall-thickness)
+(def wall-plate-u15
+  (->> (cube (+ width plate-thickness) (+ (* height 1.45) plate-thickness) wall-thickness)
        (rotate (/ Math/PI 2) [-1 0 0])
        (translate [0 (- (/ wall-thickness 2)) 0])))
 
-(def finger-cutter
-  (->> (cube width height wall-thickness)
+(def finger-cutter-u10
+  (->> (cube width height (* wall-thickness 2))
+       (rotate (/ Math/PI 2) [-1 0 0])
+       (translate [0 (- 0 wall-thickness plate-thickness) 0])))
+
+(def finger-cutter-u15
+  (->> (cube width (* height 1.45) wall-thickness)
        (rotate (/ Math/PI 2) [-1 0 0])
        (translate [0 (- 0 (/ wall-thickness 2) plate-thickness) 0])))
 
@@ -56,10 +66,22 @@
   (->> (cylinder 0.5 15)
        (rotate (/ Math/PI 2) [0 1 0])))
 
-(defn place-finger-block
+(defn place-u10-block
   [row column block]
-  (let [a (+ offset (* column step))
-        b (+ offset (* row step))
+  (let [a (+ offset-u10 (* column step-u10))
+        b (+ offset-u10 (* row step-u10))
+        x (* (Math/sin a) radius)
+        y (* (Math/cos a) radius)]
+    (->> block
+         (rotate a [0 0 -1])
+         (translate [x y 0])
+         (rotate (/ Math/PI 2) [0 1 0])
+         (rotate b [0 0 1]))))
+
+(defn place-u15-block
+  [row column block]
+  (let [a (+ offset-u10 (* column step-u10))
+        b (+ offset-u15 (* row step-u15))
         x (* (Math/sin a) radius)
         y (* (Math/cos a) radius)]
     (->> block
@@ -71,6 +93,7 @@
 (def mounting-post
   (->> (cylinder mounting-post-radius mounting-post-height)
        (with-fn 20)
+       (translate [0 0 (- plate-thickness)])
        (rotate (/ Math/PI 2) [1 0 0])))
 
 (def screw-cutter
@@ -85,7 +108,7 @@
         half-columns (/ finger-columns 2)]
     (for [row (range (- half-rows) half-rows)
           column (range (- half-columns) half-columns)]
-      (place-finger-block row column block))))
+      (place-u10-block row column block))))
 
 
 (defn place-all-thumb-blocks
@@ -94,62 +117,75 @@
         half-lower-rows (/ thumb-lower-rows 2)]
     (union
       (for [row (range (- half-upper-rows) half-upper-rows) ]
-        (place-finger-block row -0.5 upper-block))
+        (place-u10-block row -0.5 upper-block))
       (for [row (range (- half-lower-rows) half-lower-rows)]
-        (place-finger-block row 0.5 lower-block)))))
+        (place-u15-block row 0.5 lower-block)))))
 
 (defn place-all-posts
   [block]
   (union
-    (place-finger-block -0.5 0 block)
-    (place-finger-block -0.5 -1 block)
-    (place-finger-block -1.5 -2 block)
-    (place-finger-block -1.5 1 block)
-    (place-finger-block 0.5 1 block)
-    (place-finger-block 0.5 -2 block)))
+    (place-u10-block -0.5 0 block)
+    (place-u10-block -0.5 -1 block)
+    (place-u10-block -1.5 -2 block)
+    (place-u10-block -1.5 1 block)
+    (place-u10-block 0.5 1 block)
+    (place-u10-block 0.5 -2 block)))
 
 (def fingers
+  (->> 
   (difference
-    (union 
-      (place-all-finger-blocks wall-plate))
+    (union (place-all-finger-blocks wall-plate-u10)
+           )
+    (place-all-finger-blocks finger-cutter-u10)
     (place-all-finger-blocks switch-cutter)
-    (place-all-finger-blocks finger-cutter)))
+    )
+  )
+  )
 
 (def thumbs
-  (difference
     (union
-      (place-all-thumb-blocks wall-plate wall-plate-15))
-    (place-all-thumb-blocks switch-cutter switch-cutter)
-    (place-all-thumb-blocks finger-cutter finger-cutter)))
+      (place-all-thumb-blocks wall-plate-u10 wall-plate-u15)))
+
 
 (def shell-block
   (->> (cube width (+ height plate-thickness) shell-thickness)
        (rotate (/ Math/PI 2) [1 0 0])
-       (translate [0 (+ (/ shell-thickness 2) shell-gap) 0])
+       (translate [0 (+ (/ shell-thickness 2) thumb-y) 0])
        ))
 
-(defn place-row-of-blocks
-  [block]
+(defn place-row-10u-blocks
+  [block row column]
   (let [half-columns (/ finger-columns 2)]
     (for [row (range (- half-columns) half-columns)]
-    (place-finger-block row -0.5 block))))
+    (place-u10-block row -0.5 block))))
 
-(def shell-row
-  (union
-    (place-row-of-blocks shell-block)))
+(def shell-end
+(->> (difference (cylinder wall-thickness 104)
+                            (->> (cube 100 100 110)
+                                 (translate [-50 0 0])))
+                (scale [1 3 1])
+                (rotate (/ Math/PI 2) [0 1 0])
+                (translate [0 0 (- (/ width 2))])
+                (place-u10-block 1 -0.5))
+  )
 
-(def finger-shell
-  (let [shell-rows (/ shell-height width)
-        half-rows (/ shell-rows 2)]
-    (union
-      (for [row (range (- half-rows) half-rows)]
-        (->> shell-row
-             (translate [0 0 (* width row)]))))))
+(defn place-reverse-block
+  [block]
+  (->> block
+         (translate [0 (- radius) 0])
+         (rotate Math/PI [0 0 1])
+         (rotate Math/PI [0 1 0])
+         (translate [0 (+ shell-gap) 0])
+         (place-u10-block 1 -0.5))
+  )
+
 
 (def complete
   (union
     fingers 
-    finger-shell))
+    shell-end
+    (place-reverse-block thumbs)
+    ))
 
 (defn render-part!
   [[filename part]]
@@ -161,10 +197,10 @@
   (dorun 
     (map render-part!
          {"switch-cutter" switch-cutter
-          "finger-plate" finger-plate
           "fingers" fingers
           "thumbs" thumbs
-          "wall-plate" wall-plate
+          "wall-plate-u10" wall-plate-u10
+          "wall-plate-u15" wall-plate-u15
           "mounting-post" mounting-post
           "complete" complete
           })))
